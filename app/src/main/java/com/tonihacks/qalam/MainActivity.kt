632aca8f98a4788249390e1fbda704953f1eed25
Antonio Masotti <toniomasotti@gmail.com>
2026-06-28 18:02:25 +0200

    fix: edge2edge visibility

--- app/src/main/java/com/tonihacks/qalam/MainActivity.kt
+++ app/src/main/java/com/tonihacks/qalam/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.tonihacks.qalam
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
@@ -22,7 +24,10 @@ import com.tonihacks.qalam.ui.theme.QalamTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
         setContent {
             QalamTheme {
                 val backStack = remember { mutableStateListOf<Any>(Home) }
