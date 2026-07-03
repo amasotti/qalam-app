3e2fd8bc40b320057fa5bfc66d073309a9387637
Antonio Masotti <toniomasotti@gmail.com>
2026-07-03 19:32:33 +0200

    chore: separate pos and dialects

--- app/src/main/java/com/tonihacks/qalam/domain/model/Dialect.kt
+++ app/src/main/java/com/tonihacks/qalam/domain/model/Dialect.kt
@@ -1,19 +1,5 @@
 package com.tonihacks.qalam.domain.model
 
-
-enum class PartOfSpeech {
-    UNKNOWN,
-    NOUN,
-    VERB,
-    ADJECTIVE,
-    ADVERB,
-    PREPOSITION,
-    PARTICLE,
-    INTERJECTION,
-    CONJUNCTION,
-    PRONOUN;
-}
-
 enum class Dialect {
     MSA,
     TUNISIAN,
---
+++ app/src/main/java/com/tonihacks/qalam/domain/model/PartOfSpeech.kt
@@ -0,0 +1,14 @@
+package com.tonihacks.qalam.domain.model
+
+enum class PartOfSpeech {
+    UNKNOWN,
+    NOUN,
+    VERB,
+    ADJECTIVE,
+    ADVERB,
+    PREPOSITION,
+    PARTICLE,
+    INTERJECTION,
+    CONJUNCTION,
+    PRONOUN;
+}
